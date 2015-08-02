bf6849458f0ed1f0ab872c776ca14875f71ea97e
maromie <maroi.maromie@gmail.com>
2015-08-02 14:32:17 +0900

    js

---
+++ js_study/for/function.js
@@ -0,0 +1,15 @@
+window.onload = function(){
+    
+    // for Q.01
+    
+    button.onclick = function(){  
+        var i = 0;
+        for(i=0; i < 10; i++){
+             document.write(i+ "回繰り返されました。<br>");
+        }
+    }
+    
+    
+    
+
+}
