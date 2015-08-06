caad37dd9fe115e3c1f02b54853fc5a65d9537c4
maromie <maroi.maromie@gmail.com>
2015-08-07 01:20:31 +0900

    ドム②

--- js_study/dom/function.js
+++ js_study/dom/function.js
@@ -2,11 +2,27 @@ window.onload = function(){
     
     // DOM Q.01
     
-   var button = document.getElementById('button');
-   button.onclick = function(){
-
-    var text = document.getElementById('dom-text').innerText;
-    document.getElementById('result').innerHTML = text;
+    var button = document.getElementById('button');
+    button.onclick = function(){
+        
+        var text = document.getElementById('dom-text').innerText;
+        document.getElementById('result').innerHTML = text;
+        
+   }
+   
+   // DOM Q.02
+    
+    var button02 = document.getElementById('button02');
+    button02.onclick = function(){
+        
+        var text02 = document.getElementById('result02');
+        var elem = document.createElement('div');
+        
+        elem.id = 'msg';
+        elem.innerHTML = '今日は５キロ走ったよ！カポエイラやりたい！ダイエット万歳！';
+        
+        text02.appendChild(elem);
+        
    }
 
 }
