3799b79cdcde40db71908f3edb139e89f6eacf71
maromie <maroi.maromie@gmail.com>
2015-08-09 01:44:57 +0900

    ドム③

--- js_study/dom/function.js
+++ js_study/dom/function.js
@@ -16,13 +16,25 @@ window.onload = function(){
     button02.onclick = function(){
         
         var text02 = document.getElementById('result02');
-        var elem = document.createElement('div');
+        var elem = document.createElement('p');
         
         elem.id = 'msg';
         elem.innerHTML = '今日は５キロ走ったよ！カポエイラやりたい！ダイエット万歳！';
         
-        text02.appendChild(elem);
-        
+        text02.appendChild(elem);    
    }
+   
+   // DOM Q.02
+    var text03 = document.getElementById('result03');
+    var button03 = document.getElementById('button03');
+
+    button03.addEventListener('click', function(){
+
+        text03.innerHTML = '背景がバッチリ変わりましたね！';
+        text03.style.backgroundImage = 'url(images/bg01.jpg)';
+        text03.style.color = '#381a06';
+
+    },false);
+        
 
 }
